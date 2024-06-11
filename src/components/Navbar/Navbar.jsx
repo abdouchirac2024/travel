9a6f0bae40f8c546282756142334c94ee1a997ba
abdouchirac2024 <abdouchirac411@gmail.com>
2024-06-11 14:31:30 +0200

    la configuration du fichier tailwind css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbard
+    <>
+   <div className="fixed top-0 right-0 w-full bg-white text-black shadow-md">
+   <div className="container bg-gradient-to-r from-primary">
+    <div className="flex">
+    <p>20% off on next booking</p>
+    <p>Mobile No. 237658488485</p>
+
     </div>
+   </div>
+   </div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#0287a8",
+        secondary: "#00c3c7",
+      },
+      container:{
+        padding:{
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      }
+    },
   },
   plugins: [],
 }
